d2e1ded5a6afe6d729f95c13ed00a2634c3c91d0
Sofia Torgovezky <sofiat@campus.technion.ac.il>
2024-03-04 13:48:37 +0200

    new classes added: - TwoThreeTree (needed to be classic ver) - TimeTree for each Runner (will start working in it only after we had the base tree finished) - Node for the tree - Runner extends node (almost finished) - Run extends node

--- Run.java
+++ Run.java
@@ -2,11 +2,11 @@ public class Run extends Node{
     private float time;
 
 
-    public Run(float time) {
-        this.time=time;
+    public Run(float time) {this.time=time;}
+
+    public void setTime(float time) {
+        this.time = time;
     }
 
-    public float getTime() {
-        return time;
-    }
+    public float getTime() {return time;}
 }
