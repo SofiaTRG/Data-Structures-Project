f60441679f8d51fd7dc920885f32cf7e1cea5c54
nelad1997 <129320650+nelad1997@users.noreply.github.com>
2024-03-24 16:31:53 +0200

    Add files via upload
    new clases: nodefloat and mintree

---
+++ NodeFloat.java
@@ -0,0 +1,72 @@
+public class NodeFloat {
+    private float Key;
+    private TwoThreeTree<RunnerID> tree;
+    NodeFloat parent;
+    NodeFloat leftChild;
+    NodeFloat middleChild;
+    NodeFloat rightChild;
+
+    public NodeFloat(float key, NodeFloat parent, NodeFloat leftChild, NodeFloat middleChild, NodeFloat rightChild) {
+        Key = key;
+        this.parent = parent;
+        this.leftChild = leftChild;
+        this.middleChild = middleChild;
+        this.rightChild = rightChild;
+    }
+    public NodeFloat(float key) {
+        new NodeFloat(key,null,null,null,null);}
+
+    public NodeFloat(float key, TwoThreeTree<RunnerID> tree) {
+        Key = key;
+        this.tree = tree;
+    }
+
+    public float getKey() {
+        return Key;
+    }
+
+    public TwoThreeTree<RunnerID> getTree() {
+        return tree;
+    }
+
+    public NodeFloat getParent() {
+        return parent;
+    }
+
+    public NodeFloat getLeftChild() {
+        return leftChild;
+    }
+
+    public NodeFloat getMiddleChild() {
+        return middleChild;
+    }
+
+    public NodeFloat getRightChild() {
+        return rightChild;
+    }
+
+    public void setKey(float key) {
+        Key = key;
+    }
+
+    public void setTree(TwoThreeTree<RunnerID> tree) {
+        this.tree = tree;
+    }
+
+    public void setParent(NodeFloat parent) {
+        this.parent = parent;
+    }
+
+    public void setLeftChild(NodeFloat leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public void setMiddleChild(NodeFloat middleChild) {
+        this.middleChild = middleChild;
+    }
+
+    public void setRightChild(NodeFloat rightChild) {
+        this.rightChild = rightChild;
+    }
+
+}
